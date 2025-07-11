cd91ceb92d94ddf10b145e6fcc655dee97a46809
grettythunberg123 <mamorushuuya@gmail.com>
2025-07-11 17:12:50 +0900

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,1 +1,14 @@
+function checkRace() {
+  const height = document.getElementById("height").value;
+
+  if (!height) {
+    alert("Please enter your height!");
+  } else if (height < 130) {
+    alert("You might be a Hobbit! 🍃");
+  } else if (height >= 130 && height <= 180) {
+    alert("You could be a brave Human of Middle-earth!");
+  } else {
+    alert("You're tall like an Elf! 🧝‍♂️");
+  }
+}
 
